fd22fe77262d0d1a2b4d618cd8bec79f8ef52e4e
MarcosDS7 <marcossouza.0ds@gmail.com>
2022-04-02 12:06:10 -0300

    Funções com ou sem arrow function

--- src/App.js
+++ src/App.js
@@ -3,21 +3,43 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this);
+    this.state = {
+      name: 'Marcos',
+      counter: 0
+    };
+  }
+
+  handlePClick(){
+    this.setState({name: 'Mds'})
+  }
+
+  handleAClick  = (e) => {
+    e.preventDefault();
+    const { counter } = this.state;
+    this.setState({counter: counter + 1})
+  }
+
   render(){
+    const {name} = this.state;
+    const {counter} = this.state;
     return (
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={this.handlePClick}>
+         {name} {counter}
          </p>
        <a
+       onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Este é o link
         </a>
       </header>
     </div>
