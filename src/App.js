48e97834be3399db724780f30c01d2af6cbf8ea2
MarcosDS7 <marcossouza.0ds@gmail.com>
2022-04-02 12:23:14 -0300

    Método map e listando array

--- src/App.js
+++ src/App.js
@@ -1,71 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import "./App.css";
+import { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this);
-    this.state = {
-      name: 'Marcos',
-      counter: 0
-    };
-  }
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Meu Deus",
+        body: "Relaxa Marcos",
+      },
+      {
+        id: 2,
+        title: "Tá de boas",
+        body: "Eeee",
+      },
+      {
+        id: 3,
+        title: "Faz o seu",
+        body: "Falo nada",
+      },
+    ],
+  };
 
-  handlePClick(){
-    this.setState({name: 'Mds'})
-  }
+  render() {
+    const { posts } = this.state;
 
-  handleAClick  = (e) => {
-    e.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1})
-  }
-
-  render(){
-    const {name} = this.state;
-    const {counter} = this.state;
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handlePClick}>
-         {name} {counter}
-         </p>
-       <a
-       onClick={this.handleAClick}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Este é o link
-        </a>
-      </header>
-    </div>
-    )
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
